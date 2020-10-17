53d43e4a72359a210011e433e72f758e5efdbee4
gtindo <gtyletindo@gmail.com>
2020-10-17 14:27:02 +0100

    add articles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express()
 
@@ -7,6 +8,11 @@ app.use("/static", express.static('static'));
 app.get("/resume", (req, res) => {
   return res.render("resume.ejs");
 });
+
+app.get("/articles", (req, res) => {
+  let articles = JSON.parse(fs.readFileSync("articles.json").toString());
+  return res.render("articles.ejs", {articles});
+})
 
 app.get("/", (req, res) => {
   return res.render("home.ejs");
